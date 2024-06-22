18540d9c83d9728fb95ad0b6ad821579f5ea3a24
Srijan <srijan2016403221@ipu.ac.in>
2024-06-23 01:02:55 +0530

    changes

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -6,7 +6,7 @@ function Intro() {
   useEffect(() => {
     const options1 = {
       strings: ["Ciao,It's Srijan!"],
-      typeSpeed: 20,
+      typeSpeed: 50,
       showCursor: false,
       onComplete: (self) => {
         self.el.classList.remove("typing-animation");
@@ -15,7 +15,7 @@ function Intro() {
 
     const options2 = {
       strings: ["Welcome to my Portfolio"],
-      typeSpeed: 50,
+      typeSpeed: 70,
       showCursor: false,
       onComplete: (self) => {
         self.el.classList.remove("typing-animation");
